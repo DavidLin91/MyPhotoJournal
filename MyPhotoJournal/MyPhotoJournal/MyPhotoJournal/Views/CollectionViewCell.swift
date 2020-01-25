dfb9efc8278e46a908e381c90547ce276adb7b8f
David <lin.david985@gmail.com>
2020-01-25 15:03:16 -0500

    got image to load in mainVC

---
+++ MyPhotoJournal/MyPhotoJournal/MyPhotoJournal/Views/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  MyPhotoJournal
+//
+//  Created by David Lin on 1/25/20.
+//  Copyright © 2020 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
+}
--- MyPhotoJournal/MyPhotoJournal/MyPhotoJournal/Views/MainVC.swift
+++ MyPhotoJournal/MyPhotoJournal/MyPhotoJournal/Views/MainVC.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var images = [UIImage]() {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-
-
+    
+    
+    
+    @IBAction func addImageButtonPressed(_ sender: UIBarButtonItem) {
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.delegate = self
+        present(imagePickerVC, animated: true)
+    }
+    
+    @IBAction func settingsButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    
 }
 
+
+extension MainVC: UICollectionViewDelegateFlowLayout {}
+
+
+extension MainVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mainCell", for: indexPath) as? CollectionViewCell else {
+            fatalError("Could not dequeue Collection View Cell")
+        }
+        let image = images[indexPath.row]
+        cell.imageView.image = image
+        return cell
+    }
+}
+
+extension MainVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        images.append(image)
+        dismiss(animated: true, completion: nil)
+    }
+}

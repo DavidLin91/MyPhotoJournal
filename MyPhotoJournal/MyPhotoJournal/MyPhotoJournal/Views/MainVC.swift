2f51ac44e553c359bb98ea9a6d772d22f034dc2a
David <lin.david985@gmail.com>
2020-01-25 12:35:56 -0500

    edited UI for background

---
+++ MyPhotoJournal/MyPhotoJournal/MyPhotoJournal/Views/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPhotoJournal
+//
+//  Created by David Lin on 1/25/20.
+//  Copyright © 2020 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
